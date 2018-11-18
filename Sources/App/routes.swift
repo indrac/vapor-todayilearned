37610c2e8f5cc77125874b493429fb4bd23494fb
indrachyd <indraredtrlv@gmail.com>
2018-11-18 21:17:20 +0700

    Split routes out into a separate controller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,61 +3,53 @@ import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
+    
     // Basic "It works" example
     router.get { req in
         return "It works!"
     }
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
+    
+    
+    let acronymsController = AcronymsController()
+    try router.register(collection: acronymsController)
+    
+}
 
-    router.post("api", "acronyms") { req -> Future<Acronym> in
-        // 2
+
+    ////// Bellow is the old code for creating a request ///////
+    
+    /* router.post("api", "acronyms") { req -> Future<Acronym> in
         return try req.content.decode(Acronym.self)
             .flatMap(to: Acronym.self) { acronym in
-                // 3
                 return acronym.save(on: req)
         }
     }
     
-    // 1
     router.get("api", "acronyms") { req -> Future<[Acronym]> in
-        // 2
         return Acronym.query(on: req).all()
     }
-    
-    // 1
     router.get("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
-        // 2
         return try req.parameters.next(Acronym.self)
     }
     
-    // 1
     router.put("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
-        // 2
         return try flatMap(to: Acronym.self,
                            req.parameters.next(Acronym.self),
                            req.content.decode(Acronym.self)) {
                             acronym, updatedAcronym in
-                            // 3
+                            
                             acronym.short = updatedAcronym.short
                             acronym.long = updatedAcronym.long
                             
-                            // 4
                             return acronym.save(on: req)
         }
     }
     
-    // 1
     router.delete("api", "acronyms", Acronym.parameter) {
         req -> Future<HTTPStatus> in
-        // 2
         return try req.parameters.next(Acronym.self)
-            // 3
             .delete(on: req)
-            // 4
             .transform(to: HTTPStatus.noContent)
     }
     
@@ -66,38 +58,27 @@ public func routes(_ router: Router) throws {
         guard let searchTerm = req.query[String.self, at: "term"] else {
                 throw Abort(.badRequest)
         }
-        // 1
         return Acronym.query(on: req).group(.or) { or in
-            // 2
             or.filter(\.short == searchTerm)
-            // 3
             or.filter(\.long == searchTerm)
-            // 4
             }.all()
     }
     
-    // 1
     router.get("api", "acronyms", "first") { req -> Future<Acronym> in
-        // 2
         return Acronym.query(on: req)
             .first()
             .map(to: Acronym.self) { acronym in
-                // 3
                 guard let acronym = acronym else {
                     throw Abort(.notFound)
                 }
-                // 4
                 return acronym
         }
     }
     
-    // 1
     router.get("api", "acronyms", "sorted") {
         req -> Future<[Acronym]> in
-        // 2
         return Acronym.query(on: req)
             .sort(\.short, .ascending)
             .all()
-    }
-    
-}
+    } */
+
